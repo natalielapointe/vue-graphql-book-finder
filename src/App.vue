af169a56ae8ce2c77539573b64a59ea85a726968
Natalie LaPointe <hello@natalielapointe.com>
2025-04-28 14:30:25 -0500

    Add clear input field button

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,3 @@
-<template>
-  <div>
-    <h1>🌍 Country Browser</h1>
-
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="error">Error: {{ error.message }}</div>
-
-    <div v-else>
-      <label for="filter-search">filter by country name</label>
-      <input type="search" id="filter-search" v-model="searchText"></input>
-      <div v-if="!filteredCountries.length">No results</div>
-      <ul v-else>
-        <li v-for="country in filteredCountries" :key="country.code">
-          {{ country.name }} {{ country.emoji }} - ({{ country.continent.name }})
-        </li>
-      </ul>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { ref, computed } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
@@ -34,4 +14,31 @@ const filteredCountries = computed(() => {
     c.name.toLowerCase().includes(searchText.value.toLowerCase())
   )
 })
+
+const clearInputText = () => {
+  searchText.value = ""; 
+}
+
 </script>
+
+<template>
+  <div>
+    <h1>🌍 Country Browser</h1>
+
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
+
+    <div v-else>
+      <h2>Filtering options:</h2>
+      <label for="filter-search">By country name</label>
+      <input type="search" id="filter-search" v-model="searchText"></input>
+      <button type="button" @click="clearInputText">Clear</button>
+      <div v-if="!filteredCountries.length">No results</div>
+      <ul v-else>
+        <li v-for="country in filteredCountries" :key="country.code">
+          {{ country.name }} {{ country.emoji }} - ({{ country.continent.name }})
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
